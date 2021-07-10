8c8a06a6ce43d54f53067351260f5065085466b3
zlitch7 <87182179+zlitch7@users.noreply.github.com>
2021-07-10 11:18:29 +0530

    added player inventory space

--- Build Up/Assets/assets/scripts/PlayerInventory.cs
+++ Build Up/Assets/assets/scripts/PlayerInventory.cs
@@ -8,10 +8,13 @@ public class PlayerInventory : MonoBehaviour
     
     public TextMeshProUGUI blockCountText;
     public int blockCount;
+    public bool canPick = true;
+    public float InventorySpace;
 
     void Start()
     {
         blockCountText = GameObject.Find("block count text").GetComponent<TextMeshProUGUI>();
+      
         blockCount = 0;
     }
 
@@ -19,5 +22,12 @@ public class PlayerInventory : MonoBehaviour
     void Update()
     {
         //blockCountText.text  = blockCount.ToString();
+        if(blockCount >= InventorySpace){
+            canPick = false;
+        }  
+        else if(blockCount < InventorySpace){
+            canPick = true;
+        }
+        
     }
 }
